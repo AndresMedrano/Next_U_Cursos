645c9d74c5fee3d2c33b264b520739abf978259d
AndresMedrano <andres.medrano@softech.com.ec>
2017-07-13 11:09:41 -0500

    Scope de las Variables

---
+++ DesarrolladorWeb/JavaScript/ScopeVariables/js/app.js
@@ -0,0 +1,9 @@
+var mensaje ="Hola, soy un mensaje"
+
+function verMensaje() {
+	alert(mensaje) //Debe imprimir hola soy un mensaje
+	var mensaje = "Hola, soy un nuevo mensaje"
+	alert(mensaje)
+}
+
+verMensaje()
