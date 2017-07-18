738191ccc99471d56c5f0c3b67f7392df582cd2e
AndresMedrano <andres.medrano@softech.com.ec>
2017-07-18 10:39:42 -0500

    Codigo Usado para Modificar Estilo de Caja

---
+++ DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion2-ModificandoEstilosJS/ModificarEstilosdelModelodeCaja/js/ejemplo.js
@@ -0,0 +1,8 @@
+//Seleccionamos la clase card large
+document.getElementsByClassName('card large');
+
+//Modificar Margen Solido Rojo
+document.getElementsByClassName('card large')[0].style.border="3px solid red";
+
+//Modificando el Padding de la imagen
+document.getElementsByClassName('card large')[0].style.padding="20px";
