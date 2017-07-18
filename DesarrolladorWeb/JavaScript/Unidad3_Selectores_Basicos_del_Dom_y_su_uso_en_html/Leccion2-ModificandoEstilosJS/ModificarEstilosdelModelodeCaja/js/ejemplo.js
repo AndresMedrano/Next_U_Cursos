77786edef83d4267ef40dc36201722656a9f4a60
AndresMedrano <andres.medrano@softech.com.ec>
2017-07-18 10:41:28 -0500

    Agregando Comentario Estilos de Caja

--- DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion2-ModificandoEstilosJS/ModificarEstilosdelModelodeCaja/js/ejemplo.js
+++ DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion2-ModificandoEstilosJS/ModificarEstilosdelModelodeCaja/js/ejemplo.js
@@ -6,3 +6,5 @@ document.getElementsByClassName('card large')[0].style.border="3px solid red";
 
 //Modificando el Padding de la imagen
 document.getElementsByClassName('card large')[0].style.padding="20px";
+
+//Recordar que en los estilos de modelos de caja, se encuentran border, padding, margin
