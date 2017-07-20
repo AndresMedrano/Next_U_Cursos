a4fb655cb2c4dc643d439734f800f640623002b5
AndresMedrano <andres.medrano@softech.com.ec>
2017-07-20 11:21:34 -0500

    Modificando Propiedades Fondo  Color JS

---
+++ DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion2-ModificandoEstilosJS/ModificarPropiedadesColoryFondos/js/app.js
@@ -0,0 +1,5 @@
+// var listaMenu = document.getElementsByTagName('nav')[0].getElementsByTagName('li');
+//
+// for(i = 0; 1 < listaMenu.length; i++){
+//   listaMenu[i].style.borderRadius = "18px"
+// }
