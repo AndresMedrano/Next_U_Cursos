d7a48d29f5907eb04ff926519116e75da06dbb13
AndresMedrano <andres.medrano@softech.com.ec>
2017-07-20 10:33:49 -0500

    Modificando Estilos Texto JS

--- DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion2-ModificandoEstilosJS/ModificarPropiedadesdeTexto/js/selectores.js
+++ DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion2-ModificandoEstilosJS/ModificarPropiedadesdeTexto/js/selectores.js
@@ -3,3 +3,35 @@ var resultados = document.getElementsByClassName('btn_red');
 for(i=0;i<resultados.length;i++){
   resultados[0].innerHTML=texto="Nuevo texto";
 }
+
+
+//var titulos = document.getElementsByClassName('car-title') Selecciona titulos
+//titulos1[0].style.textTransform = "uppercase" Mayusculas
+//titulos1[1].style.textTransform = "lowercase" Minusculas
+//titulos1[2].style.textTransform = "capitalize" Primera Letra Mayuscula
+
+//var parrafos = document.getElementsByTagName("p") Selecciona parrafos
+//parrafos[0].style.fontFamily = "Cursive" Letra Cursiva
+//parrafos[1].style.fontFamily = "fantasy" Letra fantasy
+//parrafos[2].style.fontFamily = "monospace" Letra monospace
+
+
+//Actividad
+
+{/* <div class="row">
+<div class="col s6 m3 l3">
+  <div class="card large">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator responsive-img" src="img/clarinete.jpg" alt="Clarinete"/>
+    </div>
+    <div class="card-content">
+      <span class="card-title grey-text text-darken-4">Clarinete</span>
+      <p>El clarinete es un instrumento musical de la familia de los instrumentos de viento-madera que consta de una boquilla con caña simple.</p>
+    </div>
+  </div>
+</div>
+</div> */}
+
+// ¿Qué comandos JavaScript tengo que usar para que el título Clarinete salga en mayúscula sostenida y la descripción del instrumento en letra cursiva?
+//document.querySelector("span[class~='card-title']").style.textTransform= "uppercase";
+//document.querySelector("div[class='card-content'] p").style.fontFamily="cursive";
