8c78bc59e980de97e14eb0f022593906d538ce50
AndresMedrano <andres.medrano@softech.com.ec>
2017-09-19 13:45:19 -0500

    Eventos con Mouse

--- DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion4-EventosJS/EventosMouse/js/eventos.js
+++ DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion4-EventosJS/EventosMouse/js/eventos.js
@@ -1,1 +1,19 @@
+function cambioFotoAstronauta() {
+  document.getElementById('astronauta').style="width:50%;";
+}
+//document.getElementById('astronauta').onclick=cambioFotoAstronauta;
 
+function cambioFotoExtraterrestre() {
+  document.getElementById('extraterrestre').style="-webkit-filter:blur(5px);filter:blur(5px);";
+}
+//document.getElementById('extraterrestre').ondblclick=cambioFotoExtraterrestre;
+
+function cambioFotoPlanetas() {
+  document.getElementById('planetas').style="filter:hue-rotate(90deg);-webkit-filter:hue-rotate(90deg);";
+}
+//document.getElementById('planetas').onmouseover=cambioFotoPlanetas;
+
+function cambioFotoNave() {
+  document.getElementById('nave_espacial').style="filter:sepia(100%);-webkit-filter:sepia(100%);";
+}
+//document.getElementById('nave_espacial').onmouseout=cambioFotoNave;
