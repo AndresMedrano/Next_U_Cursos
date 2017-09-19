fd4ef8fd6c0fc93d3cd5589c8cbf1d38c73eed3b
AndresMedrano <andres.medrano@softech.com.ec>
2017-09-19 11:26:13 -0500

    Inner HTML

--- DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion3-ModificandoEstadosJS/ModificandoEstadosJS/js/selectores.js
+++ DesarrolladorWeb/JavaScript/Unidad3_Selectores_Basicos_del_Dom_y_su_uso_en_html/Leccion3-ModificandoEstadosJS/ModificandoEstadosJS/js/selectores.js
@@ -3,3 +3,17 @@ var resultados = document.getElementsByClassName('btn_red');
 for(i=0;i<resultados.length;i++){
   resultados[0].innerHTML=texto="Nuevo texto";
 }
+
+//Selectores de Formularios
+document.getElementById('animales').checked="true"
+document.getElementById('sel_animales').disabled="true"
+
+document.getElementById('sel_postres').querySelector("option[value='helado']").selected="true"
+
+document.getElementById('sel_postres').querySelector("option[value='torta_chocolate']").selected="true"
+
+$(document).ready(function()
+ {$('select').material_select();});
+
+ //innerHTML
+ document.getElementsByTagName("h5")[2].innerhtml="Selecciona tu animal favorito y tus postres preferidos.";
