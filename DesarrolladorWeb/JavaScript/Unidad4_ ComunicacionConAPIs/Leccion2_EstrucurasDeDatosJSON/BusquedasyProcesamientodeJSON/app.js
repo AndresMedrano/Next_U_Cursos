a112a8f246fe088d989ef0854157a06d8e816be6
AndresMedrano <andres.medrano@softech.com.ec>
2017-09-19 16:53:11 -0500

    Reorganizar Carpetas

---
+++ DesarrolladorWeb/JavaScript/Unidad4_ ComunicacionConAPIs/Leccion2_EstrucurasDeDatosJSON/BusquedasyProcesamientodeJSON/app.js
@@ -0,0 +1,38 @@
+var Estructura = {
+  "Personas": [
+    {
+      "Nombre": "Andrés",
+      "Edad": 28,
+      "Direccion": "Guayllabamba",
+      "Telefono": "023591094",
+      "Estudios": [
+        "Primaria Escolar": {
+          "Lugar": "Unidad Educativa Eugenio Espejo",
+          "Fecha": "1994 - 1999"
+        },
+        "Colegio" : {
+          "Lugar": "Unidad Educativa San Pedro Pascual",
+          "Fecha": "2001 - 2009"
+          }
+      ]
+    },
+    {
+      "Nombre": "Carolina",
+      "Edad": 24,
+      "Direccion": "Guayllabamba",
+      "Telefono": "023591094",
+      "Estudios": [
+        "Primaria Escolar": {
+          "Lugar": "Unidad Educativa Eugenio Espejo",
+          "Fecha": "1994 - 1999"
+          },
+        "Colegio" : {
+          "Lugar": "Unidad Educativa San Pedro Pascual",
+          "Fecha": "2001 - 2009"
+          }
+      ]
+    }
+  ]
+}
+  var persona1 = Estructura.Personas[0];
+  var persona2 = Estructura.Personas[1];
