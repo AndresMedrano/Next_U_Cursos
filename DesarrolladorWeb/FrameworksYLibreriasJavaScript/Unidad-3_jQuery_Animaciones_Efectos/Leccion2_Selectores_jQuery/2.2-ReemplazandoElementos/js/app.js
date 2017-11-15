f9da8b65beff6c9cc2901ff03cd1414d66a42af9
AndresMedrano <andres.medrano@softech.com.ec>
2017-11-15 15:17:18 -0500

    Reemplazar Elementos jQuery

--- DesarrolladorWeb/FrameworksYLibreriasJavaScript/Unidad-3_jQuery_Animaciones_Efectos/Leccion2_Selectores_jQuery/2.2-ReemplazandoElementos/js/app.js
+++ DesarrolladorWeb/FrameworksYLibreriasJavaScript/Unidad-3_jQuery_Animaciones_Efectos/Leccion2_Selectores_jQuery/2.2-ReemplazandoElementos/js/app.js
@@ -1,4 +1,5 @@
-$(function(){
+//Cambia de Color a las tarjetas
+$(function eventos(){
   $(".card").mouseover(function(){
     $(this).removeClass("blue");
     $(this).addClass("yellow");
@@ -7,14 +8,18 @@ $(function(){
     $(this).removeClass("yellow");
     $(this).addClass("blue");
   });
+})
 
 
-  $("button").on("click",function(){
+$(function remplazaElementos() {
+  $("button").on("click",function() {
+    //Reemplaza Titulo por un icono
     $("#c1").find("span").replaceWith("<i class='material-icons medium'>thumb_up</i>");
+    //Remplaza los Parrafos por un nuevo texto en formato H5
     $(".card").find("p").replaceWith("<h5> nuevo texto </h5>");
+    //Reemplaza el titulo de las tarjetas
     $("#c3").replaceWith($("#c8"));
+    //Remplaza las tarjetas por una imagen
     $("<img src='image/s7.png'/>").replaceAll($(".card"));
-
-  });
-
+  })
 })
