cee8006ba52be248f2f55b9fbd12ee5ef5b9e9f1
AndresMedrano <andres.medrano@softech.com.ec>
2017-11-15 10:31:37 -0500

    Actividad 2

--- DesarrolladorWeb/FrameworksYLibreriasJavaScript/Unidad-3_jQuery_Animaciones_Efectos/Leccion1_Efectos_para_mostrar_elementos/1.2-OcultarElementos/js/app.js
+++ DesarrolladorWeb/FrameworksYLibreriasJavaScript/Unidad-3_jQuery_Animaciones_Efectos/Leccion1_Efectos_para_mostrar_elementos/1.2-OcultarElementos/js/app.js
@@ -12,3 +12,36 @@ $("#cerdo").hide('4500','linear',function(){$("#mensaje").text("Cerdo")});
 $("#gallina").toggle();
 
 $("#oveja").toggle('500',function(){$("#mensaje").text("Oveja")});
+
+//Actividad 2
+/*$(function(){
+  $("#nombre").blur(function (){validarNombre($("#nombre").value);});
+  $("#fecha").change(function (){validarFecha($("#fecha").value);});
+  $("#email").blur(function(){
+    validarEmail($("#email").value);});
+  $("form").submit(function(){
+    alert("Ingresaste la siguiente información:"+"
+"+
+    "Nombre: "+$("#nombre").value+"
+"+
+    "Fecha: "+$("#fecha").value+"
+"+
+    "Email: "+$("email").value+"
+")
+  });
+});*/
+
+/*
+function validarNombre(nombre){
+  //...Procedimiento para validar si es una cadena de caracteres.
+  alert("El valor "+nombre+" validado.");
+};
+function validarFecha(fecha){
+  //...Procedimiento para si el valor es una fecha correcta
+  alert("El valor "+nombre+" validado.");
+};
+function validarEmail(email){
+  //... Procedimiento para validar si el valor es un email apropiado
+  alert("El valor "+nombre+" validado.");
+};
+*/
