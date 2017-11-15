68209ea1185096e61eb622817c29af1bc6cb171e
AndresMedrano <andres.medrano@softech.com.ec>
2017-11-15 10:18:49 -0500

    Mostrar Elementos jQuery

---
+++ DesarrolladorWeb/FrameworksYLibreriasJavaScript/Unidad-3_jQuery_Animaciones_Efectos/Leccion1_Efectos_para_mostrar_elementos/1.1-MostrarElementos/js/app.js
@@ -0,0 +1,13 @@
+
+$(function(){
+
+});
+
+
+$("#oveja").show('3200',function(){$("#mensaje").text("Oveja")});
+
+$("#gallina").show('slow','linear')
+
+$("#cerdo").show('5000')
+
+$("#vaca").show()
