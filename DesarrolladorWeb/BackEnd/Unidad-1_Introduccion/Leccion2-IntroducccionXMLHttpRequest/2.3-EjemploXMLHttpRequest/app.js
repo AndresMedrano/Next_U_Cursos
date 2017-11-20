a0e81fc1b79c006fac70de572217bcf6ee350130
AndresMedrano <andres.medrano@softech.com.ec>
2017-11-20 10:11:29 -0500

    Ejecuciones Sicronicas y Asincronicas

--- DesarrolladorWeb/BackEnd/Unidad-1_Introduccion/Leccion2-IntroducccionXMLHttpRequest/2.3-EjemploXMLHttpRequest/app.js
+++ DesarrolladorWeb/BackEnd/Unidad-1_Introduccion/Leccion2-IntroducccionXMLHttpRequest/2.3-EjemploXMLHttpRequest/app.js
@@ -1,14 +1,15 @@
-(function($)) {
+(function($) {
   var inputUser = $('#username')
-  var URL = 'http://api.github.com/users' inputUser.on('keypress', function(e) {
-    if (e.wich === 13) {
+  var URL = 'http://api.github.com/users/'
+  inputUser.on('keypress', function(e) {
+    if (e.which === 13) {
       var username = $(this).val().trim().toLowerCase()
       if (username !== "") {
         var XHR = new XMLHttpRequest()
         var URLUser = URL + username
         XHR.open('GET', URLUser)
         XHR.onreadystatechange = function() {
-          if (XHR.readyState === 4) {
+          if(XHR.readyState === 4) {
             var data = JSON.parse(XHR.responseText)
             if (data.message && data.message == "Not Found") {
               alert("El usuario no existe en GitHub")
