c69c6790e0c82189ca74aa7a6a15b62585592c13
AndresMedrano <andres.medrano@softech.com.ec>
2017-11-20 12:10:54 -0500

    Fetch

---
+++ DesarrolladorWeb/BackEnd/Unidad-1_Introduccion/Leccion4-UtilidadesObjetosAjax/4.3-Fetch/app.js
@@ -0,0 +1,9 @@
+for (var i = 1; i <=80 ; i++) {
+  fetch('https://pokeapi.co/api/v2/pokemon-form/' + i, {
+  method: 'GET',
+  }).then(function (data) {
+    return data.json()
+  }).then(function (json) {
+    $('.pokemons').append('<li><img src="'+ json.sprites.front_default +'"></img></li>')
+  })
+}
