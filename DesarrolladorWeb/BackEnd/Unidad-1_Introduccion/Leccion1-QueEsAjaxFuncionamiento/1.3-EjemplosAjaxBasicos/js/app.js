70338a532069aeff2a2da8d9b683bcd06b3283af
AndresMedrano <andres.medrano@softech.com.ec>
2017-11-19 20:07:30 -0500

    Introduccion XMLHttpRequest

--- DesarrolladorWeb/BackEnd/Unidad-1_Introduccion/Leccion1-QueEsAjaxFuncionamiento/1.3-EjemplosAjaxBasicos/js/app.js
+++ DesarrolladorWeb/BackEnd/Unidad-1_Introduccion/Leccion1-QueEsAjaxFuncionamiento/1.3-EjemplosAjaxBasicos/js/app.js
@@ -1,11 +1,10 @@
 var HtmlNode = document.getElementById('datos');
 
 $.ajax({
-  url: 'http://api.tvmaze.com/shows',
-  type:'GET' // Tambien puede ser POST, PUT
-  data:{},
-  success: function (data){
-    data.forEach(function (val, i) {
+  url: 'http://api.tvmaze.com/shows', type: 'GET', // Tambien puede ser POST, PUT
+  data: {},
+  success: function(data) {
+    data.forEach(function(val, i) {
       HtmlNode.innerHTML = HtmlNode.innerHTML + '<li>' + val.name + '</li>';
     })
   }
