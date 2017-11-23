fd1d8f73f023a46f8a6cb4addbc9af366294d2d3
AndresMedrano <andres.medrano@softech.com.ec>
2017-11-22 23:18:01 -0500

    Sintaxis Basica PHP

---
+++ DesarrolladorWeb/BackEnd/Unidad-3_PHP/Leccion2-SintaxisyEstructurasdeControl/2.1-SintaxisBasica/sintaxisBasica.php
@@ -0,0 +1,18 @@
+<?php
+echo "Este es un texto en PHP"; //Comentarios Lineales
+echo "Otro Texto PHP";
+/*
+Esto es un comentario
+en bloque dentro del codigo PHP
+*/
+?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sintaxis PHP</title>
+  </head>
+  <body>
+    <h1>Este Texto esta en HTML</h1>
+  </body>
+</html>
